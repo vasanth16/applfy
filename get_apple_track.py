d47b5b1c9fd5b5c7b6135ada5d88473ea1b5d0c7
Felix <felixcantdebate@gmail.com>
2018-07-09 21:52:59 -0500

    Update function to return a tuple.

--- get_apple_track.py
+++ get_apple_track.py
@@ -1,3 +1,12 @@
 #Felix Cui
 
-def get_apple_track():
+import itunes
+
+def get_apple_track(song_url):
+    ind = song_url.index('?i=') + 3 #get index of true ID
+    true_id = song_url[ind:]
+    track = itunes.lookup(int(true_id))
+
+    return (track.name, str(track.artist)[8:])
+
+#print(get_apple_track('https://itunes.apple.com/us/album/banana-breeze/1403834927?i=1403834930'))
