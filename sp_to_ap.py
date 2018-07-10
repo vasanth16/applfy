bfbc6b7405bd09431e230f291b193f00635987d8
vasanth16 <vasanthrajvr@gmail.com>
2018-07-09 21:48:52 -0400

    Finished parsing spotify link

--- sp_to_ap.py
+++ sp_to_ap.py
@@ -1,19 +1,24 @@
 import spotipy
 import requests
 from spotipy import util as util
+import json
 
 token = util.prompt_for_user_token(username="vvvv110", scope=None, client_id='b72f1627c1614421adc803989b16b19b', client_secret='268867b74222405baf2a176b3022d66e', redirect_uri='https://example.com/callback/', cache_path=None)
     #token2 = util.prompt_for_user_token(username="vvvv110", scope=None)
 
 
-def get_spotify_track():
-    yes = spotipy.Spotify(auth=token)
-    song_link = input('Please input your spotify link')
-    trackName = yes.track(song_link)
-    print(trackName['name'])
+def get_spotify_track(obj, song_link):
+    trackName = obj.track(song_link)
+    layer1 = trackName['artists']
+    layer2 = layer1[0]
+    artists = layer2['name']    
+    return (trackName['name'], artists)
     
 
+def main ():
+    yes = spotipy.Spotify(auth=token)
+    song_link = input('Please input your spotify link')
+    spotify = get_spotify_track(yes,song_link)
+    print ("Your track is",spotify[0], "by ", spotify[1])
 
-get_spotify_track()
-
-
+main()
